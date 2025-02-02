6826416a611708530e64f4517adcf2d3e70c5622
Metaphill <86081973+UEMjournal@users.noreply.github.com>
2025-02-02 05:29:57 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const translate = require('google-translate-api');
+const path = require('path');
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname));
 
 app.get('/translate', (req, res) => {
   const url = 'https://snapp.doctor/consultation/obstetrician-gynecologist/';
   const targetLang = 'ar'; // Arabic
 
   translate(url, { from: 'fa', to: targetLang }).then((translation) => {
-    res.send(translation.text);
+    res.send(`<html><body>${translation.text}</body></html>`);
   }).catch((err) => {
     console.error(err);
     res.status(500).send('Error translating page');
